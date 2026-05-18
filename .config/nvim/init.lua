6c0ed8192279980ab6cd15c7477b58a1a1528a81
yuna <76891722+ynug@users.noreply.github.com>
2026-05-18 17:13:02 +0900

    w

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -240,10 +240,10 @@ require("lazy").setup({
         yaml = { "prettier" },
         markdown = { "prettier" },
       },
-      format_on_save = {
-        timeout_ms = 2000,
-        lsp_format = "fallback",
-      },
+      -- format_on_save = {
+        -- timeout_ms = 2000,
+        -- lsp_format = "fallback",
+      -- },
     },
   },
 
